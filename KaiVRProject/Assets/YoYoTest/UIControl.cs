9bd3e3341ed7ea00e77fa52735a645a26dd95971
TOTHEMOON\youdo <1023571809@qq.com>
2025-03-17 20:37:58 +0800

    UI角度插值

---
+++ KaiVRProject/Assets/YoYoTest/UIControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public Transform targetUITransform;
+    public Transform lookTargetTransform;
+    public float lookRotateSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetUITransform != null && lookTargetTransform != null)
+        {
+            // 计算目标方向
+            Vector3 targetDirection = targetUITransform.position - lookTargetTransform.position;
+            // targetDirection.y = 0; // 保持UI在水平面上旋转
+
+            // 计算目标旋转
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+
+            // 平滑旋转到目标方向
+            targetUITransform.rotation = Quaternion.Lerp(targetUITransform.rotation, targetRotation, lookRotateSpeed * Time.deltaTime);
+        }
+    }
+}
