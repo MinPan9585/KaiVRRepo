d4601d53d16b855e82d07f67d85a1534e2673e3c
TOTHEMOON\youdo <1023571809@qq.com>
2025-04-19 19:31:23 +0800

    build create fix

--- KaiVRProject/Assets/YoYoTest/NodeScriptVR.cs
+++ KaiVRProject/Assets/YoYoTest/NodeScriptVR.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.EventSystems;
+
 
 public class NodeScriptVR : XRBaseInteractable
 {
@@ -12,11 +14,20 @@ public class NodeScriptVR : XRBaseInteractable
     private GameObject currentTurret;
     private Vector3 towerDisplace = new Vector3(0f, .3f, 0f);
 
-    protected override void Awake()
+    BuildManager buildManager;
+
+    // protected override void Awake()
+    // {
+    //     base.Awake();
+    //     rend = GetComponent<Renderer>();
+    //     originalColor = rend.material.color;
+    // }
+
+    private void Start()
     {
-        base.Awake();
         rend = GetComponent<Renderer>();
         originalColor = rend.material.color;
+        buildManager = BuildManager.instance;
     }
 
     protected override void OnHoverEntered(HoverEnterEventArgs args)
@@ -39,9 +50,21 @@ public class NodeScriptVR : XRBaseInteractable
 
     protected override void OnActivated(ActivateEventArgs args)
     {
-        Debug.LogError("OnActivated");
-        base.OnActivated(args);
-        HandleNodeClick();
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+        if (buildManager.GetTurretToBuild() == null)
+            return;
+        if (currentTurret != null)
+            return;
+        if (CoinManager.Instance.getCurrentTowerCost() > CoinManager.Instance.coins)
+            return;
+
+
+
+        Vector3 placePos = transform.position + towerDisplace;
+        GameObject turretToBuild = buildManager.GetTurretToBuild();
+        currentTurret = (GameObject)Instantiate(turretToBuild, placePos, transform.rotation);
+        CoinManager.Instance.buyTower();
     }
 
     private void HandleNodeClick()
@@ -53,5 +76,7 @@ public class NodeScriptVR : XRBaseInteractable
         Vector3 placePos = transform.position + towerDisplace;
         GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
         currentTurret = (GameObject)Instantiate(turretToBuild, placePos, transform.rotation);
+
+
     }
 }
